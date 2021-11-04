938c1ef953bd75b48ab89c0825b56ffd9e397b32
HsinChun <f107118124@nkust.edu.tw>
2021-11-04 17:05:53 +0800

    Update timestamp_fuction.py

--- timestamp_fuction.py
+++ timestamp_fuction.py
@@ -1,36 +1,29 @@
 """Run inference with a YOLOv5 model on images, videos, directories, streams
 
 Usage:
-    $ python
-     path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
+    $ python path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
 """
-import csv
-import textwrap 
-import argparse
-import sys
-import time 
-from pathlib import Path
-import cv2
-from numpy import empty
-import torch
-import torch.backends.cudnn as cudnn
-import multiprocessing as mp
+from Autoresize import *
 from convertMillis import *
 from  writecsv import get_timestamp 
+import argparse
+import sys
+import time
+from pathlib import Path
+import csv
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
 FILE = Path(__file__).absolute()
 sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
-from Music import music
+
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
     apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_sync
-import argparse
-import time
-from Autoresize import *
-#bool lock for mutilprocess
-soundlock =False
 
 
 @torch.no_grad()
@@ -40,8 +33,8 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
-        device='0',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=True,  # show results
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
         save_txt=False,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
@@ -58,7 +51,6 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
-        Run_buffer = None,
         ):
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
@@ -107,11 +99,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     if pt and device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
     t0 = time.time()
-    TemperatureWork = None
-    Run_buffer = 0
-
-    for path, img, im0s, vid_cap ,timestamp in dataset:    
-        
+    for path, img, im0s, vid_cap ,timestamp in dataset:
         if pt:
             img = torch.from_numpy(img).to(device)
             img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -126,8 +114,6 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         if pt:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(img, augment=augment, visualize=visualize)[0]
-            
-
         elif onnx:
             pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
 
@@ -141,37 +127,31 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
 
         # Process predictions
         for i, det in enumerate(pred):  # detections per image
-            
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
-                showcls= f''
             else:
                 p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
-                showcls= f''
+
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
-            
-            
             if len(det):
                 count = boundingbox = 0
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-                    showcls += f" {names[int(c)]} {n}"
-                    wrapped_text = textwrap.wrap(showcls, width=17)
+
                 # Write results
-                for *xyxy, conf, cls in reversed(det):    
+                for *xyxy, conf, cls in reversed(det):
                     c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-                    print(imc.shape[0],imc.shape[1])
                     x_1 , y_1 ,x_2,y_2= bboxautoresize(y = imc.shape[0],x = imc.shape[1])
-                    print(x_1 , y_1 ,x_2,y_2)
                     bboxCenter = [x_1, y_1, x_2, y_2]
                     bbox_2 = [c1[0], c1[1], (c2[0]-c1[0]), (c2[1]-c1[1])]
                     bbox_1 = [x_1, y_1, (x_2-x_1), (y_2-y_1)]
@@ -185,51 +165,29 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                         count+=1        
                         boundingbox +=1 
                         get_timestamp(timestamp,count,boundingbox).timestamp_()     
-                    if save_txt:  # Write to file
-                        c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
 
+
+
+
+
+                    if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        classnumber = ('%g ' * len(line)).rstrip() % line
-                        classnumber = classnumber.split(' ')
-                        c1, c2 = list(c1),list(c2)
                         with open(txt_path + '.txt', 'a') as f:
-                            #f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                            f.write(classnumber[0]+' '+ classnumber[1] +' '+ str(c1[0]) +' '+ str(c1[1]) +' '+ str(c2[0]) +' ' + str(c2[1]) + '\n')                    
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
                     if save_img or save_crop or view_img:  # Add bbox to image
-                        Run_buffer+=1
-                        # c1 = xy left top ,c2 = xy2 right bottom
-                        c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-                        x_1 , y_1 ,x_2,y_2= bboxautoresize()
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        #set center location
-                        c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-                        cv2.rectangle(im0, (bboxCenter[0], bboxCenter[1]), (bboxCenter[2], bboxCenter[3]), (0, 255, 0), 2) 
-                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness,TemperatureWork=TemperatureWork)
-                        #left top write text 
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        font_size = 1
-                        font_thickness = 2
-                        for i_local, line in enumerate(wrapped_text):
-                            textsize = cv2.getTextSize(line, font, font_size, font_thickness)[0]
-                            gap = textsize[1] + 10
-                            y = 30+ i_local * gap
-                            cv2.putText(im0, line, (3, y), font,
-                                        font_size, 
-                                        (0,0,255), 
-                                        font_thickness, 
-                                        lineType = cv2.LINE_AA)
+                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
             # Print time (inference + NMS)
-            print(f'{s}Done.. ({t2 - t1:.3f}s)')
+            print(f'{s}Done. ({t2 - t1:.3f}s)')
 
             # Stream results
             if view_img:
-                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL)
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
@@ -251,34 +209,45 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                             save_path += '.mp4'
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
-                    
+
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {colorstr('bold', save_dir)}{s}")
 
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
-    
+
     print(f'Done. ({time.time() - t0:.3f}s)')
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='./yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='20211102115600_1635829598759.mp4', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='https://www.youtube.com/watch?v=R2iMq5LKXco', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.35, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    # parser.add_argument('--view-img', action='store_true', help='show results')
+    # parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    # parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    # parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    # parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    # parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    # parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    # parser.add_argument('--augment', action='store_true', help='augmented inference')
+    # parser.add_argument('--visualize', action='store_true', help='visualize features')
+    # parser.add_argument('--update', action='store_true', help='update all models')
     parser.add_argument('--project', default='runs/detect', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
+    # parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    # parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     opt = parser.parse_args()
     return opt
-
 
 #HC add
 def get_iou(bbox_ai, bbox_gt):
@@ -295,22 +264,15 @@ def get_iou(bbox_ai, bbox_gt):
 
     return IoU
 
-
-
-
 def main(opt):
-    
     print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
     check_requirements(exclude=('tensorboard', 'thop'))
     run(**vars(opt))
 
 
 if __name__ == "__main__":
-
     with open('timestamp.csv', 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['times(ms)','總人數','未戴口罩人數'])
-    # # Run
     opt = parse_opt()
     main(opt)
-
